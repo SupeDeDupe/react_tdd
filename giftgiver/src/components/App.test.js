152c81e3295f0baad6f0d85bd211753e44b79642
Sarah Thompson <sethompson@upei.ca>
2019-05-15 12:14:50 -0300

    removed h2 from div

--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import {createSerializer} from 'enzyme-to-json';
+
+expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
 
 const app = shallow(<App />);
 
