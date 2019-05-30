5f88cac7068285b95d763489b5dd6659ae030797
Sarah Thompson <sethompson@upei.ca>
2019-05-30 11:57:28 -0300

    added test for creating Gift in App + rendered Gift in App

---
+++ giftgiver/src/components/Gift.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+class Gift extends Component{
+    constructor() {
+        super();
+        this.state = { person: '', present: ''};
+    }
+
+    render() {
+        return (
+            <div>
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Person</Form.Label>
+                        <Form.Control 
+                        className='input-person' 
+                        onChange={event => this.setState({ person: event.target.value })}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Present</Form.Label>
+                        <Form.Control 
+                        className='input-present'
+                        onChange={event => this.setState({ present: event.target.value })}
+                        />
+                    </Form.Group>
+                </Form>
+            </div>
+        )
+    }
+}
+
+export default Gift;
